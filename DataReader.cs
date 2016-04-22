f91ae145e2b66573292f75b9b311550ac371921b
Ismail NGUYEN <me@nguyenismail.com>
2016-04-22 12:17:17 +0200

    data reading in two times (first capteurs from xtim file, then values from dt files)

--- DataReader.cs
+++ DataReader.cs
@@ -1,7 +1,7 @@
 ﻿
 using SmartHome.Converters;
 using SmartHome.Models;
-using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Xml.Linq;
 
@@ -18,11 +18,22 @@ namespace SmartHome
 
         public List<Capteur> read()
         {
+            var capteurs = readCapteurs();
+
+            capteurs.ForEach(d =>
+                d.Datas = readDatas(d.Id)
+            );
+
+            return capteurs;
+        }
+
+        private List<Capteur> readCapteurs()
+        {
             var capteurs = new List<Capteur>();
 
             foreach (XElement node in _doc.Descendants("capteurs").Nodes())
             {
-                capteurs.Add(new Capteur()
+                var capteur = new Capteur()
                 {
                     Type = TypeCapteurConverter.convert(node.Attribute("type").Value),
                     Id = node.Element("id").Value,
@@ -33,12 +44,43 @@ namespace SmartHome
                         Unite = node.Element("grandeur").Attribute("unite").Value,
                         Abreviation = node.Element("grandeur").Attribute("abreviation").Value
                     },
+                    Valeur = new ValeurCapteur()
+                    {
+                        Type = node.Element("valeur").Attribute("type").Value,
+                        Min = double.Parse(node.Element("valeur").Attribute("min").Value),
+                        Max = double.Parse(node.Element("valeur").Attribute("max").Value)
+                    },
                     Box = node.Element("box").Value,
                     Lieu = node.Element("lieu").Value
-                });
+                };
+
+                foreach (XElement nodeSeuil in node.Descendants("seuils").Nodes())
+                {
+                    var seuil = new SeuilCapteur()
+                    {
+                        Description = nodeSeuil.Document.Element("seuil").Attribute("description").Value,
+                        Valeur = double.Parse(nodeSeuil.Document.Element("seuil").Attribute("valeur").Value)
+                    };
+
+                    capteur.Seuils.Add(seuil);
+                }
+
+                capteurs.Add(capteur);
             }
 
             return capteurs;
+        }
+
+        private List<SmartData> readDatas(string id)
+        {
+            var datas = new List<SmartData>();
+
+            //TO-DO:
+            //Read .dt files
+            //Get lines according to parameter 'id'
+            //Add to datas
+
+            return datas;
         }
     }
 }
